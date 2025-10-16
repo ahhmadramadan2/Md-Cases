b9410fda606a5092109601151daa3f0e422ca6b8
Ahmad Ramadan <ahmadramadan2005@icloud.com>
2025-10-17 00:13:50 +0300

    Deploy React app to main root

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter as Router } from "react-router-dom"; 
 import App from "./App.jsx";
 import "./styles/global.css";
 import { CartProvider } from "./context/CartContext.jsx";
-
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,4 +14,5 @@ createRoot(document.getElementById("root")).render(
     </CartProvider>
   </React.StrictMode>
 );
+
 
