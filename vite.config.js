76ea0cdcf54a95f6e58efcd41ebcc8f8116bfaac
Ahmad Ramadan <ahmadramadan2005@icloud.com>
2025-10-19 20:54:23 +0300

    Deploy site to root

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   base: '/Md-Cases/',
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: '/Md-Cases/',
+  base: './', // important: use relative paths when deploying to root
   plugins: [react()],
 })
+
+
