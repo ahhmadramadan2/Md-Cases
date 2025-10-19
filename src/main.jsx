874ba9c2257f36c335645d4196169d6319f997ce
Ahmad Ramadan <ahmadramadan2005@icloud.com>
2025-10-20 00:11:32 +0300

    Deploy clean build to main root (index + assets in sync)

--- src/main.jsx
+++ src/main.jsx
@@ -21,15 +21,15 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/global.css'
-// import { CartProvider } from './context/CartContext.jsx'
+import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <CartProvider> */}
+    <CartProvider>
       <Router>
         <App />
       </Router>
-    {/* </CartProvider> */}
+    </CartProvider>
   </React.StrictMode>
 )
 
